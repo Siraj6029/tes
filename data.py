e7727324c15d9f824fd66a45bf6d2d9c8b3d1e70
TOHIBA <sirajuddin6029@gmail.com>
2021-12-29 19:16:30 +0500

    after some work commit

---
+++ data.py
@@ -0,0 +1,14 @@
+posts = [
+          {
+            'author': 'siraj',
+            'title': 'any post', 
+            'content': 'first content', 
+            'posted_date': '29th dec 2021',
+           },
+           {
+            'author': 'xyz',
+            'title': 'any second post', 
+            'content': 'second content', 
+            'posted_date': '30th dec 2021',
+           }
+]
