8f7eba89bcdf1814306fbfd2527409d2f2c3319b
TOHIBA <sirajuddin6029@gmail.com>
2021-12-31 14:34:50 +0500

    after making longin.html and register.html files

--- demo.py
+++ demo.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
 from data import posts
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
+app.config['SECRETKEY'] = '5cdbe643ff49dc0edf15d972114600a0'
 
 
 @app.route("/")
@@ -13,6 +15,16 @@ def home():
 def about():
     return render_template('about.html', title='about')
 
+@app.route("/registration")
+def registration():
+    form = RegistrationForm
+    return render_template('register.html', title='Registration', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm
+    return render_template('login.html', title='Login', form=form)
+
 
 import time
 import json
@@ -20,8 +32,8 @@ import json
 def timeCurent():
     named_tuple = time.localtime() # get struct_time
     time_string = time.strftime("%m/%d/%Y, %H:%M:%S", named_tuple)
-    in_dictionary_form = {"time":time_string}
-    json_response = json.dumps(in_dictionary_form)
+    # in_dictionary_form = {"time":time_string}
+    # json_response = json.dumps(in_dictionary_form)
     return render_template('time.html', current_time = time_string)
 
 
